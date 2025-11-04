857b59b6a5d7a236f0d550e8216f19434e1e98a8
Xander P. <xandwrp@gmail.com>
2025-11-03 18:20:50 -0700

    Link to word gravity

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>xander's paradise</title>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
 <h1>xander's paradise</h1>
+
+<h3>my stuff:</h3>
+<ul>
+    <li><a href="https://wordgravity.xandwr.com/" target="_blank">Word Gravity</a></li>
+</ul>
