14e9b3291e0c430bdd4bca8f1993627cc8d4bf71
Xander P. <xandwrp@gmail.com>
2025-11-03 18:34:58 -0700

    social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <div class="bg-black/80 text-white p-8 m-4 rounded-xl border-2">
     <h1 class="text-3xl">xander's paradise</h1>
 
+    <h3 class="text-xl mt-8">my socials:</h3>
+    <ul>
+        <li class="text-blue-300 underline"><a href="https://discord.gg/r8rbntTaKQ">discord</a></li>
+        <li class="text-blue-300 underline"><a href="https://ko-fi.com/xandwr">ko-fi</a></li>
+    </ul>
+
     <h3 class="text-xl mt-8">my projects:</h3>
     <ul class="list-disc list-inside">
         <li class="text-blue-300 underline"><a href="https://wordgravity.xandwr.com/" target="_blank">Word Gravity</a></li>
