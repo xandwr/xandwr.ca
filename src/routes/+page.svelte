435857ac9a97091a3b4370ffd553d6240f5cbdf8
Xander <94504311+xandwr@users.noreply.github.com>
2025-11-18 02:50:21 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,5 +10,6 @@
     <h3 class="text-xl mt-8">my projects:</h3>
     <ul class="list-disc list-inside">
         <li class="text-blue-300 underline"><a href="https://wordgravity.xandwr.com/" target="_blank">Word Gravity</a></li>
+        <li class="text-blue-300 underline"><a href="https://www.doctown.dev/" target="_blank">Doctown</a></li>
     </ul>
 </div>
